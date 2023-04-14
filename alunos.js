16f9d687be4a7db981072decff090418a2fa0b2f
Raphaeldev84 <raphaelgomesfaria@gmail.com>
2023-04-14 09:20:53 -0300

    Segundo exercício da atividade

--- alunos.js
+++ alunos.js
@@ -2,31 +2,36 @@ const usuarios = [
     {
         nome: 'Raphael',
         email: 'raphaelgomesfaria@gmail.com',
-        media: 9.5
+        media: 9.5,
+        matricula: 1010
     }, 
 
     {
         nome: 'Renata',
         email: 'renatinha@gmail.com',
-        media: 10.0
+        media: 10.0,
+        matricula: 1015
     }, 
 
     {
         nome: 'João Pedro',
         email: 'joaopedro@gmail.com',
-        media: 8.0
+        media: 8.0,
+        matricula: 1040
     }, 
 
     {
         nome: 'Pedro Henrique',
         email: 'joaohenrique@gmail.com',
-        media: 6.0
+        media: 6.0,
+        matricula: 1080
     }, 
 
     {
         nome: 'Pedro Paulo',
         email: 'Pedro Paulo@gmail.com',
-        media: 8.0
+        media: 8.0,
+        matricula: 1050
     }, 
 
 
@@ -34,13 +39,15 @@ const usuarios = [
     {
         nome: 'José almir',
         email: 'jose.almir@soulcode.com',
-        media: 9.0
+        media: 9.0,
+        matricula: 1055
     }, 
    
     {
         nome: 'Gabriel Braga',
         email: 'gabriel.braga@soulcode.com',
-        media: 8.5
+        media: 8.5,
+        matricula: 1022
     }, 
 ]
 
--- index.js
+++ index.js
@@ -22,3 +22,23 @@ app.get("/alunos", (req, res) => {
 app.listen(3000, () => {
   console.log("Servidor rodando em http://localhost:3000/");
 });
+
+// Crie uma rota POST para “/alunos/novo” e o corpo da requisição deve conter (nome, matrícula e média). Valide os campos passados e caso contrário indique um erro (400);
+
+app.post("/alunos/novo", (req, res) => {
+  const {nome, email, matricula, media} = req.body;
+console.log(alunos)
+  if (!nome || !matricula || !media) {
+    return res.status(400).json({ error: "Todos os campos são obrigatórios "});
+  } else { 
+    alunos.push({
+      nome: nome,
+      email: email,
+      media: media,
+      matricula: matricula
+  })
+  console.log(alunos)
+    return res.status(201).json({ message: 'Aluno criado com sucesso' });
+  }
+
+})
